aac3f6d55598d156c8826325a258176cc20779ed
caremi <Willy Nilly>
2025-07-04 23:44:25 +0400

    implement route resource

--- Careminate/Routing/Route.php
+++ Careminate/Routing/Route.php
@@ -17,4 +17,11 @@ public static function middleware(array $middleware): PendingRouteGroup
     return new \Careminate\Routing\PendingRouteGroup(['middleware' => $middleware]);
 }
 
+public static function resource(string $name, string $controller): PendingResourceRegistration
+{
+    $pending = new PendingResourceRegistration($name, $controller);
+    $pending->register();
+    return $pending;
+}
+
 }
