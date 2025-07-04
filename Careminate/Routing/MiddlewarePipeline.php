c8f86418a36252becc5b7b9988073b1bbdd247f0
caremi <Willy Nilly>
2025-07-04 23:24:53 +0400

    middleware pipeline execution

---
+++ Careminate/Routing/MiddlewarePipeline.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Careminate\Routing;
+
+use Careminate\Http\Requests\Request;
+use Careminate\Http\Responses\Response;
+
+class MiddlewarePipeline
+{
+    /**
+     * @param array $middleware Fully qualified class names
+     */
+    public static function run(Request $request, array $middleware, \Closure $destination): Response
+    {
+        $pipeline = array_reduce(
+            array_reverse($middleware),
+            function ($next, $middlewareClass) {
+                return function ($request) use ($next, $middlewareClass) {
+                    if (!class_exists($middlewareClass)) {
+                        throw new \RuntimeException("Middleware class [$middlewareClass] does not exist.");
+                    }
+
+                    $middleware = new $middlewareClass();
+
+                    if (!method_exists($middleware, 'handle')) {
+                        throw new \RuntimeException("Middleware [$middlewareClass] must have a handle() method.");
+                    }
+
+                    return $middleware->handle($request, $next);
+                };
+            },
+            $destination
+        );
+
+        return $pipeline($request);
+    }
+}
