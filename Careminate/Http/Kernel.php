f0b0caf3b01f51e5334d8b55ce111c890a15a91e
caremi <Willy Nilly>
2025-07-12 16:47:10 +0400

    Add HttpKernel and route segment

--- Careminate/Http/Kernel.php
+++ Careminate/Http/Kernel.php
@@ -1,18 +1,28 @@
 <?php declare(strict_types=1);
 namespace Careminate\Http;
 
+use App\Http\HttpKernel;
 use Careminate\Routing\Router;
+use Careminate\Routing\Segment;
 use Careminate\Http\Requests\Request;
 use Careminate\Http\Responses\Response;
 
 class Kernel 
 {
-     protected Router $router;
+
+    protected Router $router;
 
     public function __construct()
     {
         $this->router = new Router();
-        $this->registerWebRoutes();
+       
+        var_dump(Segment::get(1));
+
+         if (Segment::get(1) == 'api') {
+             $this->registerApiRoutes();
+        } else {
+             $this->registerWebRoutes();
+        }
     }
 
     public function handle(Request $request): Response
@@ -22,14 +32,18 @@ class Kernel
 
     protected function registerWebRoutes(): void
     {
-        // require base_path('routes/web.php');
+         foreach (HttpKernel::$globalWeb as $global) {
+            new $global();
+        }
         require route_path('web.php');
     }
 
      protected function registerApiRoutes(): void
     {
-        // require base_path('routes/app.php');
-        require route_path('app.php');
+         foreach (HttpKernel::$globalApi as $global) {
+            new $global();
+        }
+        require route_path('api.php');
     }
 
     public function terminate(Request $request, Response $response): void
